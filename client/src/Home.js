716b26032593f88369af4d0e022da5c8bcb2f030
Lanuitjedanse <73110649+Lanuitjedanse@users.noreply.github.com>
2021-04-05 20:41:37 +0200

    calendar budget and country

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,26 +1,41 @@
 // import { useDispatch, useSelector } from "react-redux";
 // import { Link } from "react-router-dom";
 // import { useEffect, useState } from "react";
+import { useState } from "react";
 
-// import CustomElements from "./DatePicker";
+import BasicDatePicker from "./DatePicker";
 
 import RangeSlider from "./Slider";
+import SearchCountry from "./SearchCountry";
+import DateFnsUtils from "@date-io/date-fns"; // choose your lib
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 
-export default function Home() {
+export default function Home(props) {
+    const [selectedDate, handleDateChange] = useState(new Date());
+    console.log("props.value: ", props.value);
     // useEffect(() => {
     //     dispatch(showProducts());
     // }, []);
+    const getTripInfo = () => {
+        console.log("props.value: ", props.value);
+        console.log("props.value: ", selectedDate);
+    };
 
     return (
         <div className="home">
             <h1>Home component</h1>
             <section className="search-bar">
-                <p>When do you want to go?</p>
-                {/* <CustomElements /> */}
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                    <BasicDatePicker
+                        value={selectedDate}
+                        onChange={handleDateChange}
+                    />
+                </MuiPickersUtilsProvider>
 
-                <p>What's your budget</p>
-                <RangeSlider />
-                <p>Where do you want to go?</p>
+                <RangeSlider value={props.value} />
+
+                <SearchCountry />
+                <button onClick={() => getTripInfo()}>Search</button>
             </section>
         </div>
     );
